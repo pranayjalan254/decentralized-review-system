bd1ce1cfdab3fdeb87f485227fed950f4f29836d
Pranay <pranayjalan.work@gmail.com>
2025-01-27 09:49:42 +0000

    removing id_token

--- frontend/src/components/LandingPage/Hero.js
+++ frontend/src/components/LandingPage/Hero.js
@@ -14,7 +14,7 @@ export default function Hero() {
         const params = new URLSearchParams({
             client_id: clientId,
             redirect_uri: "https://true-score.vercel.app/callback",
-            response_type: "token id_token",
+            response_type: "token",
             scope: "openid email profile",
             nonce: ekp.nonce,
             prompt: "consent",
